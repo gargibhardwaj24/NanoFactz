7ea3c60870b698501d769558bf8fc2857d7b17ed
Gargi Bhardwaj <gargibhardwaj2430@gmail.com>
2025-11-29 02:28:36 +0530

    nav done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <nav className="bg-black text-white p-4 text-center font-extrabold font-Rubik fixed top-0 w-full backdrop-blur-md z-10">
+            <h1 className='text-2xl'>NanoSkillz</h1>
+        </nav>
+    )
+}
+export default Navbar;
